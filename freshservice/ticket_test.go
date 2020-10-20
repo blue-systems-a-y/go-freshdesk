354522e796cfa97a9bcb9da6ac7bd10622fd08de
danny <danny@gabriel.network>
2020-10-20 10:33:06 +0300

    minor changes

--- freshservice/ticket_test.go
+++ freshservice/ticket_test.go
@@ -1,25 +1,39 @@
 package freshservice
 
-// func TestClient_CreateTicket(t *testing.T) {
-// 	domain, apiKey := GetCredentials()
-// assert.NotEmpty(t, domain)
-// assert.NotEmpty(t, apiKey)
-// 	c := NewClient(domain, apiKey)
-// 	tkt := &Ticket{
-// 		Name:        "danny",
-// 		Email:       "danny@gabriel.network",
-// 		Subject:     "Something does not !",
-// 		Description: "Something is not working as i think it should",
-// 		Status:      2,
-// 		Priority:    2,
-// 		Source:      2,
-// 		Tags:        []string{"one"},
-// 	}
+import (
+	"testing"
 
-// 	ticket, err := c.CreateTicket(tkt)
-// 	assert.NoError(t, err)
-// 	assert.NotEmpty(t, ticket.ID)
-// }
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_CreateTicket(t *testing.T) {
+	domain, apiKey := GetCredentials()
+	assert.NotEmpty(t, domain)
+	assert.NotEmpty(t, apiKey)
+	c := NewClient(domain, apiKey)
+	// assets, err := c.ListAllAssets()
+	// assert.NoError(t, err)
+	// log.Println(assets)
+	tkt := &Ticket{
+		Name:        "danny",
+		Email:       "danny@gabriel.network",
+		Subject:     "Something does not !",
+		Description: "Something is not working as i think it should",
+		Status:      2,
+		Priority:    2,
+		Source:      2,
+		Tags:        []string{"one"},
+		Assets: []Asset{{
+			Name:      "moshe-the-shield",
+			DisplayID: 11,
+		},
+		},
+	}
+
+	ticket, err := c.CreateTicket(tkt)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, ticket.ID)
+}
 
 // func TestClient_ListAllTickets(t *testing.T) {
 // 	domain, apiKey := GetCredentials()
